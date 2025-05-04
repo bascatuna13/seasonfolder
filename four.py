aa90049cb478f6f8358ba57f67425e69020809cf
MariN <marininiashvili93@gmail.com>
2025-05-04 22:11:21 +0400

    add function of seasons

---
+++ four.py
@@ -0,0 +1,7 @@
+
+def seasons():
+    while True:
+        month = int(input("Enter number of montth"))
+        if month <1 or month>12:
+            print("enter 1-12 range")
+            continue
